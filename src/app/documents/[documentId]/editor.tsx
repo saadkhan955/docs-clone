eb1cde9613fc7fc1c89b089e9a3788b79ab2d292
Saad Khan <saad.khan955@gmail.com>
2024-12-04 16:33:49 +0530

    Align and list feature.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -11,6 +11,7 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import TextAlign from '@tiptap/extension-text-align'
 import TextStyle from '@tiptap/extension-text-style'
 import Underline from '@tiptap/extension-underline'
 import { EditorContent, useEditor } from '@tiptap/react'
@@ -64,6 +65,7 @@ export const Editor = () => {
       Underline,
       FontFamily,
       TextStyle,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
       TaskItem.configure({ nested: true, }),
       Table.configure({ resizable: true, }),
       TableRow,
