031a3e9825b86a360c4991ec440d17696779e7f7
Saad Khan <saad.khan955@gmail.com>
2024-12-05 16:40:50 +0530

    Margin ruler.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,13 +20,17 @@ import ImageResize from "tiptap-extension-resize-image"
 
 import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height'
+
 import { useEditorStore } from '@/store/use-editor-store'
+
+import { Ruler } from './ruler'
 
 export const Editor = () => {
 
   const { setEditor } = useEditorStore()
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor)
     },
@@ -98,6 +102,7 @@ export const Editor = () => {
   })
   return (
     <div className='size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
